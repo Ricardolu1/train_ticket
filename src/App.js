0710ae14bbf9bbd30a35104806877bb297c586d2
Ricardo <568082704@qq.com>
2019-10-25 00:31:23 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState,useMemo,memo,useCallback,useRef} from "react"
+import "./App.css"
 
-function App() {
+let idSeq = Date.now()
+
+const Control= memo(function Control(props) {
+  const {addTodo} = props
+  const inputRef = useRef()
+  const onSubmit = (e)=>{
+    e.preventDefault()
+    const newText =  inputRef.current.value.trim()
+    if (newText.length===0) {
+      return
+    }
+    addTodo({
+      id:++idSeq,
+      text:newText,
+      complete:false
+    })
+    inputRef.current.value = ''
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="control">
+      <h1>
+        todos
+      </h1>
+      <form onSubmit={onSubmit} >
+        <input type="text" className="new-todo" placeholder="what need to be down?" ref = {inputRef}/>
+      </form>
     </div>
-  );
+  )
+})
+
+const TodoItem = memo(function TodoItem(props) {
+  const {todo,toggleTodo,removeTodo} = props
+  const{id,text,complete} = todo
+
+  const onChange=()=>{
+    toggleTodo(id)
+  }
+  const onRemove = ()=>{
+    removeTodo(id)
+  }
+
+  return (
+    <li className="todo-item">
+      <input type="checkbox" onChange={onChange} checked={complete}/>
+      <label className={complete?'complete':''}>
+        {text}
+      </label>
+      <button onClick={onRemove}>&#xd7;</button>
+    </li>
+  )
+})
+
+
+
+const Todos =  memo(function Todos(props){
+  const {todos,toggleTodo,removeTodo} = props
+  return (
+    <ul>
+      {
+        todos.map(todo=>{
+          return <TodoItem 
+            key={todo.id}
+            todo ={todo}
+            toggleTodo={toggleTodo}
+            removeTodo={removeTodo}
+          />
+        })
+      }
+    </ul>
+  )
+})
+
+const LS_KEY = '_$-todos_'
+
+function TodoList() {
+  const [todos,setTodos] = useState([])
+
+  const addTodo = useCallback((todo)=>{
+    setTodos(todos=>[...todos,todo])
+  },[])
+
+  const removeTodo = useCallback((id)=>{
+    setTodos(todos=>todos.filter(todo=>{
+      return todo.id !==id
+    }))
+  },[])//
+  
+  const toggleTodo = useCallback((id)=>{
+    setTodos(todos => todos.map(todo=>{
+      return todo.id ===id
+          ?{
+            ...todo,
+            complete:!todo.complete,
+          }
+          :todo
+    }))
+  },[])
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem(LS_KEY)||[])
+    setTodos(todos)
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem(LS_KEY , JSON.stringify(todos))
+  },[todos])
+
+
+
+  return (
+    <div className="todo-list">
+      <Control  addTodo={addTodo}/>
+      <Todos removeTodo={removeTodo} toggleTodo={toggleTodo} todos={todos}/>
+    </div>
+  )
 }
 
-export default App;
+
+
+
+export default TodoList
