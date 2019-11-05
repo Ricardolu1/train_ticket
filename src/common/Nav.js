b388e2e8cd2106b1df7f18d5b8f57a4988d4aaad
Ricardo <568082704@qq.com>
2019-11-05 20:43:58 +0800

    完成列表展示页面

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,13 +1,57 @@
-import React from 'react'
+import React,{
+  useMemo,
+  memo
+} from 'react'
 import './Nav.css'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import useNav from '../common/useNav'
 
-export default function Nav(props) {
+const Nav = memo(function Nav(props) {
+  const {
+    date,
+    prev,
+    next,
+    isPrevDisabled,
+    isNextDisabled
+  }=props
+
+  const currentString=useMemo(()=>{
+    const d=dayjs(date)
+    return d.format('M月D日 ')+d.locale('zh-cn').format('ddd')
+    
+  },[date])
   return (
-    <div></div>
+    <div className="nav"> 
+      <span 
+        className={classnames('nav-prev',{
+          'nav-disabled':isPrevDisabled,
+        })}
+        onClick={prev}
+      >
+        前一天
+      </span>
+      <span className="nav-current">{ currentString }</span>
+      <span 
+        className={classnames('nav-next',{
+          'nav-disabled':isNextDisabled,
+        })}
+        onClick={next}
+      >
+        后一天
+      </span>
+    </div>
   )
-}
+})
 
 Nav.propTypes = {
-
+    date:PropTypes.number.isRequired,
+    prev:PropTypes.func.isRequired,
+    next:PropTypes.func.isRequired,
+    isPrevDisabled:PropTypes.bool.isRequired,
+    isNextDisabled:PropTypes.bool.isRequired,
 }
+
+export default Nav
--- src/query/App.js
+++ src/query/App.js
@@ -1,17 +1,154 @@
 import {connect} from 'react-redux'
 import './App.css'
-import React from 'react'
+import React , {useCallback,useEffect}from 'react'
 import Nav from '../common/Nav'
 import List from './List'
 import Bottom from './Bottom'
+import Header from '../common/Header'
+import URI from 'urijs'
+import {
+  setFrom,
+  setTo,
+  setHighSpeed,
+  setSearchParsed,
+  setDepartDate,
 
-
-
+  setTrainList,
+  setTicketTypes,
+  setTrainTypes,
+  setDepartStations,
+  setArriveStations,
+  prevDate,
+  nextDate
+} from './action'
+import dayjs from 'dayjs'
+import {h0} from '../common/fp'
+import useNav from '../common/useNav'
 function App(props) {
+  const { 
+    trainList, //array
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    dispatch,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd
+  }=props
+  const onBack = useCallback(()=>{
+    window.history.back()
+  },[])
+
+  useEffect(()=>{
+    const queries = URI.parseQuery(window.location.search)
+    const {
+      from,
+      to,
+      date,
+      highSpeed
+    }=queries
+    dispatch(setFrom(from))
+    dispatch(setTo(to))
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())))
+    dispatch(setHighSpeed(highSpeed==='true'))
+    dispatch(setSearchParsed(true))
+  },[])
+
+  useEffect(()=>{
+    if (!searchParsed) {
+      return
+    }
+    const url = new URI('/rest/query')
+      .setSearch('from',from)
+      .setSearch('to',to)
+      .setSearch('date',dayjs(departDate).format('YYYY-MM-DD'))
+      .setSearch('highSpeed',highSpeed)
+      .setSearch ('orderType',orderType)
+      .setSearch ('onlyTickets',onlyTickets)
+      .setSearch ('checkedTicketTypes',Object.keys(checkedTicketTypes).join())
+      .setSearch ('checkedTrainTypes',Object.keys(checkedTrainTypes).join())
+      .setSearch ('checkedDepartStations',Object.keys(checkedDepartStations).join())
+      .setSearch ('checkedArriveStations',Object.keys(checkedArriveStations).join())
+      .setSearch ('departTimeStart',departTimeStart)
+      .setSearch ('departTimeEnd',departTimeEnd)
+      .setSearch ('arriveTimeStart',arriveTimeStart)
+      .setSearch ('arriveTimeEnd',arriveTimeEnd)
+      .toString()
+    fetch(url)
+      .then(response=>response.json())
+      .then(result=>{
+        const {
+          dataMap:{
+            directTrainInfo:{
+              trains,
+              filter:{
+                ticketType,
+                trainType,
+                depStation,
+                arrStation
+              }
+            }
+          }
+        }=result
+        dispatch(setTrainList(trains))
+        dispatch(setTicketTypes(ticketType))
+        dispatch(setTrainTypes(trainType))
+        dispatch(setDepartStations(depStation))
+        dispatch(setArriveStations(arrStation))
+      })
+  },[
+      from,
+      to,
+      departDate,
+      highSpeed,
+      searchParsed,
+      orderType,
+      onlyTickets,
+      checkedTicketTypes,
+      checkedTrainTypes,
+      checkedDepartStations,
+      checkedArriveStations,
+      departTimeStart,
+      departTimeEnd,
+      arriveTimeStart,
+      arriveTimeEnd
+  ])
+
+  const {
+    isPrevDisabled,
+    isNextDisabled,
+    prev,
+    next,
+  } =useNav(departDate,dispatch,prevDate,nextDate)
+
+  if (!searchParsed) {
+    return null
+  }
+
   return (
     <div>
-      <Nav />
-      <List />
+      <div className="header-wrapper">
+        <Header title={`${from}→${to}`} onBack={onBack}/>
+      </div>
+      <Nav 
+        date={departDate}
+        isPrevDisabled={isPrevDisabled}
+        isNextDisabled={isNextDisabled}
+        prev={prev}
+        next={next}
+      />
+      <List 
+        list={trainList}
+      />
       <Bottom />
     </div>
   )
