5e3616a56f934c0a8ae44c31fe35eeb367b84f67
Ricardo <568082704@qq.com>
2019-11-07 23:27:45 +0800

    初始化数据

--- src/ticket/store.js
+++ src/ticket/store.js
@@ -10,8 +10,18 @@ import thunk from 'redux-thunk'
 export default createStore(
   combineReducers(reducers),
   {
-
-  },
+    departDate:Date.now(),
+    arriveDate:Date.now(),
+    departTimeStr:null,
+    arriveTimeStr:null,
+    departStation:null,
+    arriveStation:null,
+    trainNumber:null,
+    durationStr:null,
+    tickets:[], //座次和出票渠道
+    isScheduleVisible:false,
+    searchParsed:false, //url解析是否完成，，为true时我们才能进行一步数据请求，以及渲染
+  },//暂时我们的想到的就这11个state
   applyMiddleware(thunk)
 )
 
